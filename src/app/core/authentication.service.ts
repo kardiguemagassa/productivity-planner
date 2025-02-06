0c8c8c9ab6e59209ddd1a6d354e8606562dad14f
kardiguemagassa <129184661+kardiguemagassa@users.noreply.github.com>
2025-02-06 10:47:57 +0100

    Live coding n°9: Walking Skeleton x Firebase Store

--- src/app/core/authentication.service.ts
+++ src/app/core/authentication.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.dev';
 import { Observable } from 'rxjs';
@@ -15,6 +15,17 @@ interface FirebaseResponseSignup {
   expiresIn: string;
   localId: string;
 }
+
+interface FirebaseResponseSignin {
+  displayName: string;
+  email: string;
+  expiresIn: string;
+  idToken: string;
+  localId: string;
+  refreshToken: string;
+  registered: boolean;
+}
+
 
 @Injectable({
   providedIn: 'root'
@@ -34,4 +45,32 @@ export class AuthenticationService {
     };
     return this.#http.post<FirebaseResponseSignup>(url, body);
   }
+
+  login(email: string, password: string): Observable<FirebaseResponseSignin> {
+    const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseConfig.apiKey}`;
+    const body = { email, password, returnSecureToken: true };
+
+    return this.#http.post<FirebaseResponseSignin>(url, body);
+  }
+
+  save(email: string, userId: string, bearerToken: string): Observable<unknown> {
+    
+    const baseUrl = `https://firestore.googleapis.com/v1/projects/${environment.firebaseConfig.projectId}/databases/(default)/documents`;
+    const userFirestoreCollectionId = 'users';
+    const url = `${baseUrl}/${userFirestoreCollectionId}?key=${environment.firebaseConfig.apiKey}&documentId=${userId}`;
+    const body = {
+      fields: {
+        email: { stringValue: email },
+      },
+    };
+
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${bearerToken}`,
+    });
+    const options = { headers };
+
+    return this.#http.post(url, body, options);
+  }
+
+
 }
