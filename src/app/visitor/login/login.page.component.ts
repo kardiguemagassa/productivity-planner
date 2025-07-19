aa6f6e85b6566f93231a8e59dd5d7e169688a06a
kardiguemagassa <129184661+kardiguemagassa@users.noreply.github.com>
2025-07-19 13:55:30 +0200

    wip(lint): apply onPush change detection strategy on missing components

--- src/app/visitor/login/login.page.component.ts
+++ src/app/visitor/login/login.page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { InvalidCredentialError } from './domain/invalid-credential.error';
 import { LoginUserUseCase } from './domain/login-user.use-case';
@@ -6,7 +6,8 @@ import { LoginUserUseCase } from './domain/login-user.use-case';
 @Component({
   imports: [FormsModule],
   templateUrl: './login.page.component.html',
-  styleUrl: './login.page.component.scss'
+  styleUrl: './login.page.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoginPageComponent {
   readonly #loginUserUseCase = inject(LoginUserUseCase);
