59cbda7e2fe472490bc146d5c6c1f05e7edf68cb
kardiguemagassa <129184661+kardiguemagassa@users.noreply.github.com>
2025-11-26 11:30:02 +0100

    feat: user can start a pomodoro + refactor task components

--- src/app/membership/workday/task-readonly/task-readonly.dumb.component.spec.ts
+++ src/app/membership/workday/task-readonly/task-readonly.dumb.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TaskReadonlyDumbComponent } from './task-readonly.dumb.component';
+import { getEmptyTask } from '../workday.page.store';
 
 describe('TaskReadonlyDumbComponent', () => {
   let component: TaskReadonlyDumbComponent;
@@ -8,12 +9,14 @@ describe('TaskReadonlyDumbComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TaskReadonlyDumbComponent]
-    })
-    .compileComponents();
+      imports: [TaskReadonlyDumbComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TaskReadonlyDumbComponent);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('task', getEmptyTask());
+    fixture.componentRef.setInput('index', 0);
     fixture.detectChanges();
   });
 
--- src/app/membership/workday/task-readonly/task-readonly.dumb.component.ts
+++ src/app/membership/workday/task-readonly/task-readonly.dumb.component.ts
@@ -1,12 +1,25 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  input,
+} from '@angular/core';
+import { getPomodoroListEmojiStatus, Task } from '../task.model';
 
 @Component({
   selector: 'app-task-readonly',
   imports: [],
   templateUrl: './task-readonly.dumb.component.html',
-  styleUrl: './task-readonly.dumb.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '[attr.data-testid]': '`task-${index()}`',
+  },
 })
 export class TaskReadonlyDumbComponent {
+  readonly task = input.required<Task>();
+  readonly index = input.required<number>();
 
+  readonly pomodoroListEmojiStatus = computed(() => {
+    return getPomodoroListEmojiStatus(this.task().pomodoroList);
+  });
 }
